1e61e7ea20781ebff2248f72086b65c5fa928eef
gabriel168 <gabriel.dettling@gmail.com>
2017-03-09 19:25:02 +0100

    fix rendering (but not movement)

--- app/Main.hs
+++ app/Main.hs
@@ -30,16 +30,17 @@ main = SDL.withInit [SDL.InitEverything] $ do
     go keysDown screen s wpos wdir = do
         keysDown' <- parseEvents keysDown
         (ds, s') <- stepSession s
+        --------
         (ex, wp') <- stepWire wpos ds (Right keysDown')
-	(ed, wdir') <- stepWire wdir ds (Right keysDown')
+        (ed, wdir') <- stepWire wdir ds (Right keysDown')
         let p = either (const 0) id ex
         let (x,y) = toCrapCoordinates (p ^._x, p ^._y)
-	let dir = either (const 0) id ed
-	let (or_x, or_y) = toCrapCoordinates (f*(dir ^._x), f*(dir ^._y)) where f = 1.0 :: Double
-	--putStrLn (show or_x)	
+        let dir = either (const 0) id ed
+        let (or_x, or_y) = (f*(dir ^._x), f*(dir ^._y)) where f = 10.0 :: Double
+        --putStrLn (show or_x)	
         (SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 10 10 10 >>= SDL.fillRect screen Nothing
         (SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 0 50 200 >>= SDL.fillRect screen (Just $ SDL.Rect (round x-25) (round (y-25)) 50 50)
-	(SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 0 200 0 >>= SDL.fillRect screen (Just $ SDL.Rect (round (x-15+or_x)) (round (y-25+or_y)) 30 30)
+        (SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 0 200 0 >>= SDL.fillRect screen (Just $ SDL.Rect (round (x-15+or_x)) (round (y-15+or_y)) 30 30)
 
         SDL.flip screen
         SDL.delay (1000 `div` 60)
@@ -58,19 +59,20 @@ keyDown :: SDL.SDLKey -> Set SDL.Keysym -> Bool
 keyDown k = not . null . filter ((== k) . SDL.symKey)
 
 pos :: (Monad m, HasTime t s) => Wire s () m (Set SDL.Keysym) (V2 Double)
-pos = integrateVec (V2 0 0) . vel
+pos = integrateVec (V2 100 100) . acc
 
 vel :: (Monad m, HasTime t s) => Wire s () m (Set SDL.Keysym) (V2 Double)
 vel = integrateVec (V2 0 0) . acc
 
 acc :: (Monad m, HasTime t s) => Wire s () m (Set SDL.Keysym) (V2 Double)
 acc  = fmap (*thrust) dir_Vec . when (keyDown SDL.SDLK_UP)
-             <|> pure (V2 0 0)
+        <|> fmap (*(-thrust)) dir_Vec . when (keyDown SDL.SDLK_DOWN)     
+        <|> pure (V2 0 0)
 
 turning_rate :: (Monad m, HasTime t s) => Wire s () m (Set SDL.Keysym)  Double
-turning_rate = pure (agility) . when (keyDown SDL.SDLK_LEFT) 
-		<|> pure (-agility) . when (keyDown SDL.SDLK_RIGHT) 
-		<|> pure 0
+turning_rate = pure (-agility) . when (keyDown SDL.SDLK_LEFT) 
+        <|> pure (agility) . when (keyDown SDL.SDLK_RIGHT) 
+        <|> pure 0
 
 orientation :: (Monad m, HasTime t s) => Wire s () m (Set SDL.Keysym)  Double
 orientation = integral (0) . turning_rate
