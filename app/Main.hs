72593b322a1fb8c3392a41fcee79a97152a21119
gabriel168 <gabriel.dettling@gmail.com>
2017-03-19 18:13:02 +0100

    less verbose names

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE RankNTypes #-} --forall
 import Prelude hiding ((.), id, null, filter)
 import Control.Monad (void)
 import Control.Wire hiding (empty)
@@ -30,49 +29,48 @@ parseEvents keysDown = do
         SDL.KeyUp k -> parseEvents (delete k keysDown)
         _ -> parseEvents keysDown
 
-keyDown :: SDL.SDLKey -> Set SDL.Keysym -> Bool
-keyDown k = not . null . filter ((== k) . SDL.symKey)
+keyDown' :: SDL.SDLKey -> Set SDL.Keysym -> Bool
+keyDown' k = not . null . filter ((== k) . SDL.symKey) 
 
 deriving instance Ord SDL.Keysym
 
 --Core Logic
 data GameState = GameState{
-    rocket_Acc :: V2 Double,
-    rocket_Vel :: V2 Double,
-    rocket_Pos :: V2 Double, 
-    rocket_Orientation :: Double
+    acc :: V2 Double,
+    vel :: V2 Double,
+    pos :: V2 Double, 
+    orientation :: Double
 }
 
 start :: GameState
 start = GameState{
-    rocket_Acc = V2 0 0,
-    rocket_Vel = V2 0 40,
-    rocket_Pos = V2 100 100,
-    rocket_Orientation = 0
+    acc = V2 0 0,
+    vel = V2 0 40,
+    pos = V2 100 100,
+    orientation = 0
 }
 
 runGame :: (Monad m, HasTime t s) => GameState -> Wire s () m (Set SDL.Keysym) GameState
-runGame prevF = mkPure $ \ds ks -> (Right prevF, runGame (next ds ks))   
-    where next ds ks = let dtv = realToFrac (dtime ds) :: V2 Double
-                           dts = realToFrac (dtime ds) :: Double
-                           pressed = flip keyDown $ ks
+runGame prevF = mkPure $ \ds keys -> (Right prevF, runGame (next ds keys))   
+    where next ds ks = let keyDown = flip keyDown' $ ks
+                           dt = realToFrac (dtime ds) :: Double
                            gravity = V2 0 0
-                           engine_acc = (*engine_Power) . angle . rocket_Orientation $ prevF
-                           engine_Power = if pressed (SDL.SDLK_UP) then thrust 
-                                          else if pressed (SDL.SDLK_DOWN) then (-thrust)
+                           engine_acc = (*engine_Power) . angle . orientation $ prevF
+                           engine_Power = if keyDown (SDL.SDLK_UP) then thrust 
+                                          else if keyDown (SDL.SDLK_DOWN) then (-thrust)
                                           else 0
-                           omega = if pressed (SDL.SDLK_LEFT) then agility
-                                   else if pressed (SDL.SDLK_RIGHT) then (-agility)
+                           turning_rate = if keyDown (SDL.SDLK_LEFT) then agility
+                                   else if keyDown (SDL.SDLK_RIGHT) then (-agility)
                                    else 0
                      in GameState{
-                            rocket_Acc = engine_acc + gravity,
-                            rocket_Vel = rocket_Vel prevF + (dtv*rocket_Acc prevF),
-                            rocket_Pos = rocket_Pos prevF + (dtv*rocket_Vel prevF),
-                            rocket_Orientation = rocket_Orientation prevF + dts*omega
+                            acc = engine_acc + gravity,
+                            vel = vel prevF + (dt *^ acc prevF),
+                            pos = pos prevF + (dt *^ vel prevF),
+                            orientation = orientation prevF + dt * turning_rate
                         }
 
 gameW = runGame start
-        
+
 --Graphics Rendering
 toCrapCoordinates :: (Num a) => (a,a) -> (a,a)
 toCrapCoordinates (x,y) = (x,(fromIntegral height)-y)
@@ -83,15 +81,15 @@ render screen game = do
     (SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 10 10 10 >>= SDL.fillRect screen Nothing
     
     --Rocket
-    let p = rocket_Pos game
+    let p = pos game
         (x,y) = toCrapCoordinates (p ^._x, p ^._y)
     (SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 0 50 200 >>= SDL.fillRect screen (Just $ SDL.Rect (round x-25) (round (y-25)) 50 50)
     --Orientation Indicator
-    let  dir = angle . rocket_Orientation $ game
+    let  dir = angle . orientation $ game
          (dir_x, dir_y) = (f*(dir ^._x), -f*(dir ^._y)) where f = 10.0 :: Double
     (SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 0 200 0 >>= SDL.fillRect screen (Just $ SDL.Rect (round (x-10+dir_x)) (round (y-10+dir_y)) 20 20)
     --Velocity Indicator
-    let v = rocket_Vel game
+    let v = vel game
         (vel_x, vel_y) = (f*(v ^._x), -f*(v^._y)) where f = 1
     (SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 200 0 0 >>= SDL.fillRect screen (Just $ SDL.Rect (round (x-10+vel_x)) (round (y-10+vel_y)) 20 20)  
     
